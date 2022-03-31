ad78d3983cc94b5ef9c973e61011fb5c25dbccc0
Val <82764150+hopkee@users.noreply.github.com>
2022-03-31 21:09:02 +0300

    New project

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Valya on 31.03.22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
